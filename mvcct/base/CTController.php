4263096ea62d69626a0e790b2d8e3d46be0a3ec3
trungnt2791 <trungnt2791@gmail.com>
2013-10-25 16:37:09 +0700

    commit CTController.php

--- mvcct/base/CTController.php
+++ mvcct/base/CTController.php
@@ -4,4 +4,20 @@ class CTController{
     function actionIndex(){
         echo 'this will be default index of the BASE Controller class';
     }
-}
+    
+    function loadModel($model){
+        $path = 'models/' . $model . '.php';
+        if(file_exists($path)){
+            require $path;
+            $this->model = new $model();
+        }else{
+            Bootstrap::error();
+        }
+    }
+    
+    function render($viewFile,$data){
+        extract($data);
+        require 'views/' . $viewFile . '.php';
+    }
+    
+}   
