1968f57a940c9047d1843a17f8b3e44c77fc3178
DuyHT91 <duyht@DuyHTs-MacBook-Pro.local>
2013-10-28 20:01:02 +0700

    update db, added protected/models/User.php and protected/controllers/UserController.php

---
+++ protected/controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * UserController 
+ * 
+ * @author duyht <duyht@smartosc.com>
+ * @created 28 Oct 2013
+ * @copyright &copy; 2013 Createve Team 
+ */
+
+//fuck this shit! im so tired right now :P
+
+
+class UserController extends CTController{
+    $id = 1;
+    public function actionTestGetUser($id) {
+        //$model = $this->loadModel('User');
+        //$userData = $model->getUser($id);
+        //this->render
+        
+        if (!empty($id)) {
+            $model = $this->loadModel('User');
+            $row = $model->getUser($id);
+            if (empty($row)) {
+                Bootstrap::error('404');
+            } else {
+                $userData = $row;
+                //not done yet
+            }
+        }else{
+            header("Location: http://irene.local/");
+        }
+    }
+}
