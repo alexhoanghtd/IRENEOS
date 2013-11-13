da4f0801a3cf04669b0c92677a04a5ca81e6848f
Alex Hoang <alexhoang.htd@gmail.com>
2013-11-13 11:20:48 +0700

    update category update

--- protected/views/collection/products.php
+++ protected/views/collection/products.php
@@ -1,35 +1,56 @@
 <div class="content-inner single-col">
-    <div class="collection-header">
-        <div class="shadow-box"
+    <div class="collection-header clearfix">
+        <div class="shadow-box header-cover"
              style="background-image:"></div>
-        <h1>Category Name</h1>
+        <div class="collection-details">
+            <h1>Collection Name</h1>
+            <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</blockquote>
+        </div>
+        
     </div>
-    <div class="product-list">
+    <hr>
+    <div class="collection-product-list">
         <ul>
             <li>
-                <form>
-                    <div class="main-view shadow-box empty pic-input">
-                        <img height="100%">
-                        <input type="file" multiple="" accept="image/*" name="cover" class="file" onchange="preview(this)">
+                <form class="clearfix collection-product-box">
+                    <div class="collection-product-cover">
+                        <div class="shadow-box empty pic-input">
+                            <img height="100%">
+                            <input type="file" accept="image/*" name="cover" class="file" onchange="preview(this)">
+                        </div>
                     </div>
-                    <div>Product Name</div>
-                    <div class="actions">
+                    <div class="selected-product">
+                        <h2>Product Name</h2>
+                    </div>
+                    <div class="item-actions">
                         <input type="submit" value="Update">
-                        <a href="#">delete</a>
+                        <a href="#" class="delete-link">delete</a>
+                    </div>
+                </form>
+            </li>
+            <hr/>
+            <li>
+                <form class="clearfix collection-product-box add-item">
+                    <div class="collection-product-cover">
+                        <div class="shadow-box empty pic-input">
+                            <img height="100%">
+                            <input type="file" accept="image/*" name="cover" class="file" onchange="preview(this)">
+                        </div>
+                    </div>
+                    <div class="selected-product">
+                        <h2>Product Name</h2>
+                    </div>
+                    <div class="item-actions">
+                        <input type="submit" value="Add Product">
+                        <a href="#" class="delete-link">delete</a>
                     </div>
                 </form>
             </li>
         </ul>
     </div>
-    <form>
-        <div class="main-view shadow-box empty pic-input">
-            <img height="100%">
-            <input type="file" multiple="" accept="image/*" name="cover" class="file" onchange="preview(this)">
-        </div>
-        <div>Product Name</div><input type="hidden" name="product_id" value ="">
-        <div class="actions">
-            <input type="submit" value="Update">
-            <a href="#">delete</a>
-        </div>
-    </form>
 </div>
