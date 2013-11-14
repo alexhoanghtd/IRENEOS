1dabca8086c17379c7ba38c4f67a0d859074b59e
Alex Hoang <alexhoang.htd@gmail.com>
2013-11-14 17:03:38 +0700

    yolo :D

--- protected/views/bill/view.php
+++ protected/views/bill/view.php
@@ -63,9 +63,99 @@
     </div>
     <hr>
     <div class="bill-items">
-
+    <h1>Products Infomations</h1>
+    <table>
+        <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Size</th>
+            <th>Color</th>
+            <th>Quantity</th>
+            <th>Price ($)</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nina Black</td>
+            <td>XL</td>
+            <td>Black</td>
+            <td>1</td>
+            <td>220</td>
+        </tr>
+    </table>
     </div>
+    <hr>
     <div class="bill-bottom-line">
-
+        <h1>Summary</h1>
+        <ul>
+            <li>
+                <h3>Sub Total: </h3><span>$400</span>
+            </li>
+            <li>
+                <h3>Tax: </h3><span>10%</span>
+            </li>    
+            <li>
+                <h3>Shipping fee: </h3><span>20$</span>
+            </li>
+            <hr>
+            <li class="bill-total"> 
+                <h3>Total: </h3><span>Trung shit tính đê :))</span>
+            </li>
+        </ul>
     </div>
 </div>
