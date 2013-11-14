b3ceaaccbff5b3b5c753b6d27992c0d1512f933c
Alex Hoang <alexhoang.htd@gmail.com>
2013-11-14 23:41:14 +0700

    update view for product/view.php

--- js/elements.js
+++ js/elements.js
@@ -7,8 +7,8 @@ function searchStyle() {
         document.getElementById("user-search-output").innerHTML = "";
     });
 }
-function ajaxSearch(input){
-    $("#user-search-output").load("http://irene.local/product/AjaxSearch/",{"name": input.value});
+function ajaxSearch(input) {
+    $("#user-search-output").load("http://irene.local/product/AjaxSearch/", {"name": input.value});
 }
 function preview(input) {
     parent = input.parentNode
@@ -18,30 +18,36 @@ function preview(input) {
     fReader.onloadend = function(event) {
         img.src = event.target.result;
         var css = parent.getAttribute("class");
-        css = css.replace('empty','');
-        parent.setAttribute("class",css);
+        css = css.replace('empty', '');
+        parent.setAttribute("class", css);
     }
 }
 
-function viewOver(input){
-    var url = input.getAttribute;
-    alert(url);
+function viewOver(input, productId) {
+    var url = $(input).css('background-image');
+    darkBoxOpen();
+    $(".dark-box-container").load('http://irene.local/picture/Slide/' + productId, function() {
+        $(".slide-view").css("background-image", url);
+    });
+
+
 }
 
-function darkBoxClose(){
+function darkBoxClose() {
     $(".dark-box").fadeOut(200);
+    $(".dark-box-container").html("");
 }
-function darkBoxOpen(){
+function darkBoxOpen() {
     $(".dark-box").fadeIn(200);
 }
 
-function ajaxBootstrap(){
+function ajaxBootstrap() {
     //darkBox = $(".dark-box-ajax");
-    $(".dark-box-ajax").click(function(e){
+    $(".dark-box-ajax").click(function(e) {
         e.preventDefault();
         darkBoxOpen();
         var url = $(e.target).attr('href');
         //alert(url);
-        $(".dark-box-container").load('http://irene.local'+url);
+        $(".dark-box-container").load('http://irene.local' + url);
     });
 }
---
+++ protected/views/picture/slider.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="<?php echo ct::baseURL() ?>/css/slider.css">
+
+<script type="text/javascript">
+    
+</script>
+
+<div class="slider-container">
+    <div class="slide-left"></div>
+    <div class="slide-view">
+        
+    </div>
+    <div class="slide-right"></div>
+</div>
